fa8df6168b17fa6372455bd7d4b7a5bea3d494ff
gz-lenk <gz595740361@outlook.com>
2020-04-20 17:19:12 +0000

    用户微博列表

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,6 +10,7 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(UserTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
 
         Model::reguard();
     }
